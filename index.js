f9b5c8b0c5235d3d90b13b14397ca532478c1d5d
Mekyi <651682+Mekyi@users.noreply.github.com>
2019-11-17 18:53:06 +0200

    Added error prevention when profile not found/private

--- index.js
+++ index.js
@@ -21,13 +21,26 @@ $('a[target=_blank]').on('click', function(){
 });
 
 $('#search').click(() => {
+  // Clear existing search results
+  clearSearch();
+  
   data.steamid = $('#username-input').val();
   
   $.post(LOCAL_SERVER_URL + '/OwnedGames', data, (res, status) => {
-    console.log(`${res} and ${status}`)
-    if (status == 'success') {
+    console.log(`${res} ${typeof(res)} and ${status}`)
+    let response = tryParseJSON(res);
+    console.log(response);
+    if (status == 'success' && response) {
       userGameList = JSON.parse(res);
-      createGameTable(userGameList['response']['games']);
+      if (userGameList['response']['games']) {
+        createGameTable(userGameList['response']['games']);
+      }
+      else {
+        spawnAlert("Profile is set to private!");
+      }
+    }
+    else {
+      spawnAlert("Profile not found!");
     }
   })
 })
@@ -58,5 +71,31 @@ let createGameTable = (array) => {
       </tr>`
     );
   });
-  $("#game-table").show();
+  $('#game-table').show();
+}
+
+let tryParseJSON = (jsonString) => {
+  try {
+    let o = JSON.parse(jsonString);
+    if (o && typeof o === "object") {
+      return o;
+    }
+  }
+  catch (e) { }
+
+  return false;
+}
+
+let spawnAlert = (alertText, alertType="danger") => {
+  $('#alerts').append(
+    `<div class="alert alert-${alertType} fade show" role="alert">
+      ${alertText}
+    </div>`
+  )
+}
+
+let clearSearch = () => {
+  $('#alerts div').alert('close');
+  $('#game-table').hide();
+  $('#game-table-body tr').remove();
 }

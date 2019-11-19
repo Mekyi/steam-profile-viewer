694e12120f28d3190e6b004592d4c4feec21b07e
RabbitTank <Mikael.Petrow@student.lamk.fi>
2019-11-19 08:33:10 +0200

    Added route test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var chai = require('chai');
 var app = require('../main.js');
+var supertest = require('supertest');
 
 global.app = app;
 global.expect = chai.expect;
@@ -18,5 +19,16 @@ describe('Input validation', function() {
   });
 
 // Test if we are getting something
+describe('GET /tasks', function() {
+  it('returns a list of tasks', function(done) {
+      request.get('/tasks')
+          .expect(200)
+          .end(function(err, res) {
+              expect(res.body).to.have.lengthOf(2);
+              done(err);
+          });
+  });
+});
+
 
 // Tests if we are getting correct information from API

5c1421da70ce5c949d6eaf9705e26c1893b29e2b
Mekyi <651682+Mekyi@users.noreply.github.com>
2019-11-21 20:46:10 +0200

    Hiding API key

--- test/test.js
+++ test/test.js
@@ -16,9 +16,9 @@ describe('API key', function() {
   it('tests if API key exists', function() {
     var isTrue = true;
     if (app.Key != null){
-      isTrue = true
+      isTrue = false
     }
-    else{isTrue = false}
+    else{isTrue = true}
     expect(isTrue).to.be.true;
     
   });
